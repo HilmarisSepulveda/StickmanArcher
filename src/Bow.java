aa7bdf5273eaf7fe0cd8bdb3fa50315277af698f
HilmarisSepulveda <hilmaris.sepulveda@gmail.com>
2018-10-29 23:35:00 -0400

    Creating base classes

---
+++ src/Bow.java
@@ -0,0 +1,20 @@
+
+public class Bow {
+	
+	private double angle;
+	private Point location;
+	
+	public double getAngle() {
+		return angle;
+	}
+	public void setAngle(double angle) {
+		this.angle = angle;
+	}
+	public Point getLocation() {
+		return location;
+	}
+	public void setLocation(Point location) {
+		this.location = location;
+	}
+
+}
--- src/Stickman.java
+++ src/Stickman.java
@@ -1,7 +1,30 @@
 
 public class Stickman {
 	
-	
-	
+	private int health;
+	private Point location;
 
+
+	public int getHealth() {
+		return health;
+	}
+
+
+	public void setHealth(int health) {
+		this.health = health;
+	}
+
+
+	public Point getLocation() {
+		return location;
+	}
+
+
+	public void setLocation(Point location) {
+		this.location = location;
+	}
+	
+	
 }
+
+
