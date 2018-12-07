701c4524ffe6bd17bfd65a8170113d082298516b
HilmarisSepulveda <hilmaris.sepulveda@gmail.com>
2018-12-06 23:58:09 -0400

    ah

--- src/game/Client.java
+++ src/game/Client.java
@@ -3,19 +3,13 @@ package game;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
 import java.net.ConnectException;
 import java.net.Socket;
 import java.net.SocketException;
 import java.util.Scanner;
-
-import javax.swing.JOptionPane;
-
-import entity.Player;
-import geometry.Point;
 import utility.ClientUtilities;
-import utility.ServerUtilities;
+
 
 public class Client {
 	/**
